73cc25a427fa9e6480abd0090ed5a68206a7d7df
Christian Tavares <christiandstavares@outlook.com>
2019-11-03 23:20:20 -0300

    implementação do calendário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGrigPlugin from '@fullcalendar/timegrid';
+import bootstrapPlugin from '@fullcalendar/bootstrap';
+import { EventInput } from '@fullcalendar/core/structs/event';
+import locale from '@fullcalendar/core/locales/pt-br';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'calendario';
+
+    title = 'calendario';
+    calendarPlugins = [dayGridPlugin, timeGrigPlugin, bootstrapPlugin];
+    events: EventInput[] = [
+        { title: 'Evento 1', date: new Date('2019-11-01T14:00:00'), allDay: false },
+        { title: 'Evento 2', date: new Date(), allDay: true }
+    ];
+    locale = locale;
+    headerDefault = {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'timeGridDay,timeGridWeek,dayGridMonth'
+    };
+    header: any = this.headerDefault;
+
+    toogleHeader(): void {
+        if (this.header) {
+            this.header = false;
+        } else {
+            this.header = this.headerDefault;
+        }
+    }
 }
